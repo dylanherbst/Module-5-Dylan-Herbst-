19efdfd3998c7d02a0d9dae5d5dbd45744a88222
dylanherbst <dylan.herbstwork@gmail.com>
2024-01-23 17:10:38 +0800

    e7

--- index.js
+++ index.js
@@ -5,12 +5,19 @@ const userRoutes = require('./routes/userRoutes');
 
 const testRoutes = require('./routes/myTestRoutes');
 
+
 const express = require('express')
 
 const app = express()
 const port = 3000
 
 app.use(express.json());
+
+const swaggerUi = require('swagger-ui-express');
+swaggerDocument = require('./swagger.json');
+app.use( '/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
 
 app.get('/E1', (req, res) => {
     res.send('Exercise 1 Example')
--- routes/calcRoute.js
+++ routes/calcRoute.js
@@ -1,48 +1,25 @@
 
 const express = require('express');
 const router = express.Router();
+const calcControl = require("../controller/calcController");
 
-// router.get('/add', (req, res) => {
-//     console.log(req.query)
-// res.send(req.query)
-//     res.send('Ready to Add')
-//     })
 
     router.get('/add', (req, res) => {
-        let number1 = parseInt(req.query.num1);
-        let number2 = parseInt(req.query.num2);
-        let sum = number1 + number2
-        console.log(sum)
-        res.status(200)
-        res.json({result:sum})
+        calcControl.addNumbers(req,res)
         })
 
+
     router.get('/subtract', (req, res) => {
-        let number1 = parseInt(req.query.num1);
-        let number2 = parseInt(req.query.num2);
-        let sum = number1 - number2
-        console.log(sum)
-        res.status(200)
-        res.json({result:sum})
+        calcControl.takeNumbers(req,res)
        })
 
 
        router.get('/divide', (req, res) => {
-        let number1 = parseInt(req.query.num1);
-        let number2 = parseInt(req.query.num2);
-        let sum = number1 / number2
-        console.log(sum)
-        res.status(200)
-        res.json({result:sum})
+        calcControl.divideNumbers(req,res)
        })
 
        router.get('/multiply', (req, res) => {
-        let number1 = parseInt(req.query.num1);
-        let number2 = parseInt(req.query.num2);
-        let sum = number1 * number2
-        console.log(sum)
-        res.status(200)
-        res.json({result:sum})
+        calcControl.multiplyNumbers(req,res)
         })         
     
 
